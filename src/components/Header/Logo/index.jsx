e7fd74af8b90638ac2660db75514b36b64df7a9e
hidepon503 <blue.passion625@gmail.com>
2024-07-06 22:45:08 +0900

    all

---
+++ src/components/Header/Logo/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './style.module.scss'
+
+const Logo = () => {
+  return (
+    <div>
+      <h1 className={styles.test} >morita</h1>
+      <p>kaeru</p>
+    </div>
+  )
+}
+
+export default Logo
